27b08f69d430d2650d1624f7f6351962d8ee1327
johnosmond <john@jtech10.com>
2023-06-03 14:08:12 -0400

    adds a flavors list view (empty for now)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,26 +11,29 @@
                 <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <i class="fa fa-address-book" aria-hidden="true"></i>
-                        {{ __("Stores") }}
+                        {{ __('Stores') }}
                     </div>
-                    <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-gray-900 dark:text-gray-100">
-                            <i class="fa fa-address-book" aria-hidden="true"></i>
-                            {{ __("Flavors") }}
-                        </div>
-                        <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                            <div class="p-6 text-gray-900 dark:text-gray-100">
-                                <i class="fa fa-address-book" aria-hidden="true"></i>
-                                {{ __("Calenar") }}
-                            </div>
-                        </div>
-                        <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
-                            <div class="p-6 text-gray-900 dark:text-gray-100">
-                                <i class="fa fa-address-book" aria-hidden="true"></i>
-                                {{ __("Users") }}
-                            </div>
-                        </div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <i class="fa fa-address-book" aria-hidden="true"></i>
+                        {{ __('Flavors') }}
+                    </div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <i class="fa fa-address-book" aria-hidden="true"></i>
+                        {{ __('Calenar') }}
+                    </div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <i class="fa fa-address-book" aria-hidden="true"></i>
+                        {{ __('Users') }}
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('flavors.index')" :active="request()->routeIs('flavors.index')">
+                        {{ __('Flavors') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
