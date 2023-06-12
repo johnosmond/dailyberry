e1440e15cc00edec7aa290773a1c710879d8e2b1
johnosmond <john@jtech10.com>
2023-06-11 22:40:27 -0400

    changes some css to improve the look on small screens

--- resources/views/flavors/index.blade.php
+++ resources/views/flavors/index.blade.php
@@ -13,10 +13,10 @@
         }
     </script>
     <div class="pt-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <form name="frmSearch" id="frmSearch" method="GET" action="{{ route('flavors.index') }}">
-                <div class="flex gap-4">
-                    <div class="w-1/2">
+                <div class="flex gap-2 md:gap-4">
+                    <div class="w-full md:w-1/2">
                         <input class="input" type="text" name="flavor" id="flavor" placeholder="Search by Flavor"
                             value="{{ request('flavor') }}" />
                     </div>
@@ -26,9 +26,9 @@
                     <div class="">
                         <button id="btnClear" class="btn" type="button" onclick="clearFlavor()">Clear</button>
                     </div>
-                    <div class="w-1/2 text-right pr-10">
+                    <div class="md:w-1/2 text-right pr-2 md:pr-10">
                         <label for="display_count" class="text-gray-400 hidden md:inline-block">Display:</label>
-                        <select name="display_count" id="display_count" class="w-24 rounded-md border-gray-400 text-gray-400"
+                        <select name="display_count" id="display_count" class="select"
                             value="{{ $display_count }}" onchange="submitAfterDisplayChange()">
                             <option value="10" {{ $display_count == '10' ? 'selected' : '' }}>10</option>
                             <option value="20" {{ $display_count == '20' ? 'selected' : '' }}>20</option>
@@ -47,11 +47,11 @@
                     <table class="table-fixed w-full mb-6">
                         <thead>
                             <tr class="text-gray-500">
-                                <th class="w-[30%] px-4 py-2 text-xl font-bold md:w-[20%]"><a
-                                        href="{{ route('flavors.index') . '?sort=flavor' . $dirstr }}">Flavor</a></th>
-                                <th class="w-[70%] px-4 py-2 text-xl font-bold">Description</th>
-                                <th class="w-[10%] px-4 py-2 font-bold hidden md:table-cell"><a
-                                        href="{{ route('flavors.index') . '?sort=times_used' . $dirstr }}"># <span
+                                <th class="w-[30%] px-4 py-2 md:w-[20%]"><a
+                                        href="{{ route('flavors.index') . '?sort=flavor' . $dirstr }}" class="text-base md:text-lg font-bold">Flavor</a></th>
+                                <th class="w-[70%] px-4 py-2"><span class="text-base md:text-lg font-bold">Description</span></th>
+                                <th class="w-[10%] px-4 py-2 hidden md:table-cell"><a
+                                        href="{{ route('flavors.index') . '?sort=times_used' . $dirstr }}" class="text-base md:text-lg font-bold"># <span
                                             class="hidden lg:inline-block">Used</span></a>
                                 </th>
                             </tr>
@@ -59,9 +59,9 @@
                         <tbody>
                             @foreach ($flavors as $flavor)
                                 <tr class="odd:bg-gray-100">
-                                    <td class="border px-4 py-2">{{ $flavor->flavor }}</td>
-                                    <td class="border px-4 py-2">{{ $flavor->description }}</td>
-                                    <td class="border px-4 py-2 hidden md:table-cell">{{ $flavor->times_used }}</td>
+                                    <td class="text-sm md:text-base border px-4 py-2">{{ $flavor->flavor }}</td>
+                                    <td class="text-sm md:text-base border px-4 py-2">{{ $flavor->description }}</td>
+                                    <td class="text-sm md:text-base border px-4 py-2 hidden md:table-cell">{{ $flavor->times_used }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
