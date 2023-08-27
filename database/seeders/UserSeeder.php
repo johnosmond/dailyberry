3ec24b72b31e6b7317efcc81494cc32044103ad5
johnosmond <john@jtech10.com>
2023-08-26 23:25:19 -0400

    This is getting frustrating.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,48 +13,46 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $password = 'XPeX2Wr$CjPg';
-        $hash = password_hash($password, PASSWORD_BCRYPT);
         User::create([
             'name' => 'John',
             'email' => 'support@sweetberries.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         User::create([
             'name' => 'Jane',
             'email' => 'jane@sweetberries.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         User::create([
             'name' => 'Janice',
             'email' => 'janice@sweetberries.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         User::create([
             'name' => 'Gian',
             'email' => 'gian@sweetberries.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         User::create([
             'name' => 'Cindy',
             'email' => 'cin4gators@gmail.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         User::create([
             'name' => 'Brandon',
             'email' => 'gatoroz@yahoo.com',
             'email_verified_at' => now(),
-            'password' => $hash,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
     }
 }
