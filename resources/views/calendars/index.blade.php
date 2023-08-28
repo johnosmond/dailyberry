32a4ad8f9ab01b18a1d871cbe9060fa2f55dda10
johnosmond <john@jtech10.com>
2023-08-28 12:30:21 -0400

    Adds the first calendar view.

---
+++ resources/views/calendars/index.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Calendars') }}
+    </x-slot>
+    <div class="pt-4">
+        <div class="main-level-1">
+            <div class="main-level-2">
+                <div class="main-level-3">
+                    <p>Calendars will go here.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
